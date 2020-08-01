40b52ca8e976afbf697dc6331b36f2e6a67ec7be
Yuji Kan <gtenio@gmail.com>
2020-08-01 20:13:49 +0900

    Updates

---
+++ precache-manifest.46a123303146e4fe37b1c5c6f4507d92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "546b9b0b29119be1880171df11bd9af3",
+    "url": "/github-pages-create-react-app/index.html"
+  },
+  {
+    "revision": "1b6fdc2494d612021c36",
+    "url": "/github-pages-create-react-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ef3a00054b35b43e2e2c",
+    "url": "/github-pages-create-react-app/static/js/2.7255cc2e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-pages-create-react-app/static/js/2.7255cc2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b6fdc2494d612021c36",
+    "url": "/github-pages-create-react-app/static/js/main.b77f73f0.chunk.js"
+  },
+  {
+    "revision": "f3d1193c854dd36e6557",
+    "url": "/github-pages-create-react-app/static/js/runtime-main.a87b8d72.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-pages-create-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
